fb5efe4962c671816c24c9ae794671fa2eadfcdd
Martin Swinkels <martin.swinkels@msc365.eu>
2025-10-07 15:48:22 +0100

    feat: Add ado/git cmdlet for managing repositories

---
+++ src/MSc365.Idp.Toolbox/ado/git/repositories/public/getRepository.ps1
@@ -0,0 +1,73 @@
+﻿function Get-AdoRepository {
+    <#
+    .SYNOPSIS
+        Get the repository.
+
+    .DESCRIPTION
+        This function retrieves the repository details as GitRepository object for an Azure DevOps repository through REST API.
+
+    .PARAMETER ProjectId
+        Mandatory. The unique identifier or name of the project.
+
+    .PARAMETER Name
+        Mandatory. The name or ID of the repository.
+
+    .LINK
+        https://learn.microsoft.com/en-us/rest/api/azure/devops/git/repositories/get-repository
+
+    .EXAMPLE
+        $repo = Get-AdoRepository -ProjectName 'my-project-001' -Name 'my-project-001'
+    #>
+    [CmdletBinding()]
+    [OutputType([pscustomobject])]
+    param (
+        [Parameter(Mandatory)]
+        [Alias('ProjectName')]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [Parameter(Mandatory = $false)]
+        [Alias('Api')]
+        [ValidateSet('7.1', '7.2-preview.1')]
+        [string]$ApiVersion = '7.1'
+    )
+
+    begin {
+        Write-Verbose ('Command      : {0}' -f $MyInvocation.MyCommand.Name)
+        Write-Verbose ('  ProjectId  : {0}' -f $ProjectId)
+        Write-Verbose ('  Name       : {0}' -f $Name)
+        Write-Verbose ('  ApiVersion : {0}' -f $ApiVersion)
+    }
+
+    process {
+        try {
+            $ErrorActionPreference = 'Stop'
+
+            if (-not $global:AzDevOpsIsConnected) {
+                throw 'Not connected to Azure DevOps. Please connect using Connect-AdoOrganization.'
+            }
+
+            $uriFormat = '{0}/{1}/_apis/git/repositories/{2}?api-version={3}'
+            $azDevOpsUri = ($uriFormat -f [uri]::new($global:AzDevOpsOrganization), [uri]::EscapeDataString($ProjectId), [uri]::EscapeDataString($Name), $ApiVersion)
+
+            $params = @{
+                Method  = 'GET'
+                Uri     = $azDevOpsUri
+                Headers = $global:AzDevOpsHeaders
+            }
+
+            $response = Invoke-RestMethod @params -Verbose:$VerbosePreference
+
+            return $response
+
+        } catch {
+            throw $_
+        }
+    }
+
+    end {
+        Write-Verbose ('Exit : {0}' -f $MyInvocation.MyCommand.Name)
+    }
+}
---
+++ src/MSc365.Idp.Toolbox/ado/git/repositories/public/newRepository.ps1
@@ -0,0 +1,97 @@
+﻿function New-AdoRepository {
+    <#
+    .SYNOPSIS
+        Create a new repository in an Azure DevOps project.
+
+    .DESCRIPTION
+        This function creates a new repository in an Azure DevOps project through REST API.
+
+    .PARAMETER ProjectId
+        Mandatory. The unique identifier or name of the project.
+
+    .PARAMETER Name
+        Mandatory. The name of the repository.
+
+    .PARAMETER SourceRef
+        Optional. Specify the source refs to use while creating a fork repo.
+
+    .PARAMETER ApiVersion
+        Optional. The API version to use.
+
+    .LINK
+        https://learn.microsoft.com/en-us/rest/api/azure/devops/git/repositories/create?view=azure-devops
+
+    .EXAMPLE
+        $repo = New-AdoRepository -ProjectId $project.id -Name 'my-other-001'
+    #>
+    [CmdletBinding()]
+    [OutputType([pscustomobject])]
+    param (
+        [Parameter(Mandatory)]
+        [Alias('ProjectName')]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [Parameter(Mandatory = $false)]
+        [string]$SourceRef,
+
+        [Parameter(Mandatory = $false)]
+        [Alias('Api')]
+        [ValidateSet('7.1', '7.2-preview.2')]
+        [string]$ApiVersion = '7.1'
+    )
+
+    begin {
+        Write-Verbose ('Command      : {0}' -f $MyInvocation.MyCommand.Name)
+        Write-Verbose ('  ProjectId  : {0}' -f $ProjectId)
+        Write-Verbose ('  Name       : {0}' -f $Name)
+        Write-Verbose ('  SourceRef  : {0}' -f $SourceRef)
+        Write-Verbose ('  ApiVersion : {0}' -f $ApiVersion)
+    }
+
+    process {
+        try {
+            $ErrorActionPreference = 'Stop'
+
+            if (-not $global:AzDevOpsIsConnected) {
+                throw 'Not connected to Azure DevOps. Please connect using Connect-AdoOrganization.'
+            }
+
+            try {
+                [System.Guid]::Parse($ProjectId) | Out-Null
+            } catch {
+                $ProjectId = (Get-AdoProject -ProjectName $ProjectId).id
+            }
+
+            $uriFormat = '{0}/{1}/_apis/git/repositories?sourceRef={2}&api-version={3}'
+            $azDevOpsUri = ($uriFormat -f [uri]::new($global:AzDevOpsOrganization), $ProjectId, $SourceRef, $ApiVersion)
+
+            $body = @{
+                name    = $Name
+                project = @{
+                    id = $ProjectId
+                }
+            } | ConvertTo-Json -Depth 5
+
+            $params = @{
+                Method  = 'POST'
+                Uri     = $azDevOpsUri
+                Headers = $global:AzDevOpsHeaders
+                Body    = $body
+            }
+
+            $response = Invoke-RestMethod @params -ContentType 'application/json' -Verbose:$VerbosePreference
+
+            return $response
+
+        } catch {
+            throw $_
+        }
+    }
+
+    end {
+        Write-Verbose ('Exit : {0}' -f $MyInvocation.MyCommand.Name)
+    }
+}
