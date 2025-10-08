7404eb990c596564755f1c6dc8180eb9f7e6fd6b
Martin Swinkels <120459644+MSc365Admin@users.noreply.github.com>
2025-10-08 20:32:10 +0200

    feat: Add Get-AdoContext function (#30)

---
+++ src/MSc365.Idp.Toolbox/ado/helper/public/getContext.ps1
@@ -0,0 +1,36 @@
+﻿function Get-AdoContext {
+    <#
+    .SYNOPSIS
+    #>
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param ()
+
+    begin {
+        Write-Debug ('{0} entered' -f $MyInvocation.MyCommand)
+    }
+
+    process {
+        try {
+            $ErrorActionPreference = 'Stop'
+
+            $isConnected = Get-Variable -Name 'AzDevOpsIsConnected' -Scope Global -ErrorAction SilentlyContinue
+            $organization = Get-Variable -Name 'AzDevOpsOrganization' -Scope Global -ErrorAction SilentlyContinue
+
+            if ($null -ne $isConnected -and $isConnected.Value -and $null -ne $organization) {
+                return @{
+                    Connected    = $isConnected.Value
+                    Organization = $organization.Value
+                }
+            } else {
+                return $null
+            }
+        } catch {
+            throw $_
+        }
+    }
+
+    end {
+        Write-Debug ('{0} exited' -f $MyInvocation.MyCommand)
+    }
+}
