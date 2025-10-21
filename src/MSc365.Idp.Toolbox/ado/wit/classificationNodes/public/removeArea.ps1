a1ed6c7e4ac4402dd1e331edfb48c16167554853
Martin Swinkels <martin.swinkels@msc365.eu>
2025-10-21 16:14:05 +0100

    feat: Add Remove-AdoArea function to delete area classification nodes in Azure DevOps

---
+++ src/MSc365.Idp.Toolbox/ado/wit/classificationNodes/public/removeArea.ps1
@@ -0,0 +1,85 @@
+﻿# cSpell: words classificationnodes
+function Remove-AdoArea {
+    <#
+    .SYNOPSIS
+        Removes an area classification node from a project in Azure DevOps.
+
+    .DESCRIPTION
+        This function removes an area classification node from a specified project in Azure DevOps using the REST API.
+
+    .PARAMETER Path
+        Required. The path of the area node to remove. The root area node cannot be removed.
+
+    .PARAMETER ProjectId
+        Mandatory. The ID or name of the Azure DevOps project.
+
+    .PARAMETER ApiVersion
+        Optional. The API version to use.
+
+    .LINK
+        https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/classification-nodes/delete
+
+    .NOTES
+        - Requires an active connection to Azure DevOps using Connect-AdoOrganization.
+
+    .EXAMPLE
+        Remove-AdoArea -ProjectId 'my-project-001' -Path 'Area/SubArea'
+
+        This example removes the area node at the specified path from the specified project.
+
+    .EXAMPLE
+        Remove-AdoArea -ProjectId 'my-project-001' -Path 'Area'
+
+        This example removes the area node named 'Area' from the specified project including its 'SubArea' child node.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Path,
+
+        [Parameter(Mandatory)]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory = $false)]
+        [Alias('api')]
+        [ValidateSet('7.1', '7.2-preview.2')]
+        [string]$ApiVersion = '7.1'
+    )
+
+    begin {
+        Write-Verbose ('Command      : {0}' -f $MyInvocation.MyCommand.Name)
+        Write-Verbose ('  Path       : {0}' -f $Path)
+        Write-Verbose ('  ProjectId  : {0}' -f $ProjectId)
+        Write-Verbose ('  ApiVersion : {0}' -f $ApiVersion)
+    }
+
+    process {
+        try {
+            $ErrorActionPreference = 'Stop'
+
+            if (-not $global:AzDevOpsIsConnected) {
+                throw 'Not connected to Azure DevOps. Please connect using Connect-AdoOrganization.'
+            }
+
+            $uriFormat = '{0}/{1}/_apis/wit/classificationnodes/Areas/{2}?api-version={3}'
+            $azDevOpsUri = ($uriFormat -f [uri]::new($global:AzDevOpsOrganization), [uri]::EscapeUriString($ProjectId), [uri]::EscapeUriString($Path), $ApiVersion)
+
+            $params = @{
+                Method  = 'DELETE'
+                Uri     = $azDevOpsUri
+                Headers = ((ConvertFrom-SecureString -SecureString $global:AzDevOpsHeaders -AsPlainText) | ConvertFrom-Json -AsHashtable)
+            }
+
+            $response = Invoke-RestMethod @params -Verbose:$VerbosePreference
+
+            return $response
+
+        } catch {
+            throw $_
+        }
+    }
+
+    end {
+        Write-Verbose ('{0} exited' -f $MyInvocation.MyCommand)
+    }
+}
