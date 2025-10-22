906b060571ae93cca09f8c2c6717b0a336ab0178
Martin Swinkels <120459644+MSc365Admin@users.noreply.github.com>
2025-10-22 15:49:03 +0100

    feat: Add support for managing iteration (#43)
    - docs: Update commands documentation to include AdoClassificationNode cmdlets
    - feat: Update module manifest to include AdoClassificationNode cmdlets
    - feat: Add Set-AdoClassificationNode function to update areas and iteration nodes
    - feat: Add Remove-AdoClassificationNode function to delete areas and iteration nodes
    - feat: Add New-AdoClassificationNode function to create areas and iteration nodes
    - feat: Add Get-AdoClassificationNode function to retrieve areas and iteration nodes

--- src/MSc365.Idp.Toolbox/ado/wit/classificationNodes/classificationNodes.psd1
+++ src/MSc365.Idp.Toolbox/ado/wit/classificationNodes/classificationNodes.psd1
@@ -9,7 +9,7 @@
 @{
 
     # Script module or binary module file associated with this manifest.
-    RootModule = 'classificationNodes.psm1'
+    RootModule        = 'classificationNodes.psm1'
 
     # Version number of this module.
     ModuleVersion     = '0.1.0'
@@ -70,10 +70,10 @@
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
     FunctionsToExport = @(
-        'Get-AdoArea'
-        'New-AdoArea'
-        'Set-AdoArea'
-        'Remove-AdoArea'
+        'Get-AdoClassificationNode'
+        'New-AdoClassificationNode'
+        'Set-AdoClassificationNode'
+        'Remove-AdoClassificationNode'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ src/MSc365.Idp.Toolbox/ado/wit/classificationNodes/public/setClassificationNode.ps1
@@ -0,0 +1,102 @@
+﻿# cSpell: words classificationnodes
+function Set-AdoClassificationNode {
+    <#
+    .SYNOPSIS
+        Updates a classification node for a project in Azure DevOps.
+
+    .DESCRIPTION
+        This function updates the name of a classification node for a specified project in Azure DevOps using the REST API.
+
+    .PARAMETER ProjectId
+        Mandatory. The ID or name of the Azure DevOps project.
+
+    .PARAMETER StructureType
+        Mandatory. The type of classification node to update. Valid values are 'Areas' or 'Iterations'.
+
+    .PARAMETER Name
+        Mandatory. The new name for the classification node.
+
+    .PARAMETER Path
+        Optional. The path of the classification node to update. If not specified, the root classification node is updated.
+
+    .PARAMETER ApiVersion
+        Optional. The API version to use.
+
+    .LINK
+        https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/classification-nodes/create-or-update
+
+    .NOTES
+        - Requires an active connection to Azure DevOps using Connect-AdoOrganization.
+
+    .EXAMPLE
+        $updatedAreaNode = Set-AdoClassificationNode -ProjectId 'my-project-001' -Name 'New Area Name' -Path 'Area/SubArea'
+
+        This example updates the name of the specified area node.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('Areas', 'Iterations')]
+        [string]$StructureType,
+
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Path,
+
+        [Parameter(Mandatory = $false)]
+        [Alias('api')]
+        [ValidateSet('7.1', '7.2-preview.2')]
+        [string]$ApiVersion = '7.1'
+    )
+
+    begin {
+        Write-Verbose ('Command         : {0}' -f $MyInvocation.MyCommand.Name)
+        Write-Verbose ('  ProjectId     : {0}' -f $ProjectId)
+        Write-Verbose ('  StructureType : {0}' -f $StructureType)
+        Write-Verbose ('  Name          : {0}' -f $Name)
+        Write-Verbose ('  Path          : {0}' -f $Path)
+        Write-Verbose ('  ApiVersion    : {0}' -f $ApiVersion)
+    }
+
+    process {
+        try {
+            $ErrorActionPreference = 'Stop'
+
+            if (-not $global:AzDevOpsIsConnected) {
+                throw 'Not connected to Azure DevOps. Please connect using Connect-AdoOrganization.'
+            }
+
+            $uriFormat = '{0}/{1}/_apis/wit/classificationnodes/{2}/{3}?api-version={4}'
+            $azDevOpsUri = ($uriFormat -f [uri]::new($global:AzDevOpsOrganization), [uri]::EscapeUriString($ProjectId),
+                $StructureType, [uri]::EscapeUriString($Path), $ApiVersion)
+
+            $body = @{
+                name = $Name
+            } | ConvertTo-Json
+
+            $params = @{
+                Method      = 'PATCH'
+                Uri         = $azDevOpsUri
+                ContentType = 'application/json'
+                Headers     = ((ConvertFrom-SecureString -SecureString $global:AzDevOpsHeaders -AsPlainText) | ConvertFrom-Json -AsHashtable)
+                Body        = $body
+            }
+
+            $response = Invoke-RestMethod @params -Verbose:$VerbosePreference
+
+            return $response
+
+        } catch {
+            throw $_
+        }
+    }
+
+    end {
+        Write-Verbose ('{0} exited' -f $MyInvocation.MyCommand)
+    }
+}
